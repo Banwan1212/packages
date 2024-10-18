b53198ffc8affdd557d3b6946adc4b67c026f548
banwan1212 <109507355+Banwan1212@users.noreply.github.com>
2024-10-18 12:22:55 +0800

    Update netdata.lua

--- luci-app-netdata/luasrc/model/cbi/netdata/netdata.lua
+++ luci-app-netdata/luasrc/model/cbi/netdata/netdata.lua
@@ -16,6 +16,14 @@ o=s:option(Value, "port",translate("Set the netdata access port"))
 o.datatype="uinteger"
 o.default=19999
 
+local link = s:option(DummyValue, "_link", translate("Web NetData"))
+link.rawhtml = true
+link.cfgvalue = function(self, section)
+    local port = self.map:get(section, "port") or "19999"
+    local ip = luci.sys.exec("ubus call network.interface.lan status | jsonfilter -e '@[\"ipv4-address\"][0].address'")
+    return string.format("<a href='http://%s:%s' target='_blank'>点击这里访问NetData</a>", ip, port)
+end
+
 local e=luci.http.formvalue("cbi.apply")
 if e then
   io.popen("/etc/init.d/netdata start")
